f9ebcf4e0c46fa7286ea829f105d8962c3f9e9a0
ajinas <ajinas@qburst.com>
2016-07-18 14:04:32 +0530

    animation given to pop-up

--- script/housing.js
+++ script/housing.js
@@ -2,15 +2,20 @@ window.onload = function(){
 	document.getElementById('second').addEventListener("click", function(){
 		var loc = document.getElementById('pop');
 		if(loc.style.display === 'block'){
-			loc.style.display = 'none';
-			//document.getElementById('sam').style.animation = 'animr .5s'
+			document.getElementById('primary').style.display = 'inline';
+			document.getElementById('secondary').style.display = 'none';
+			document.getElementById('sam').style.animation = 'animr .25s';
+
+			setTimeout(function(){ loc.style.display = 'none'; }, 250);
 		}
 		else{
 			loc.style.display = 'block';
-			//document.getElementById('sam').style.animation = 'animf .5s'
+			document.getElementById('primary').style.display = 'none';
+			document.getElementById('secondary').style.display = 'inline';
+			document.getElementById('sam').style.animation = 'animf .25s';
 			
 		}
 
-		
+		//setTimeout(function(){ loc.style.display = 'none'; }, 400);
 	});
 }
