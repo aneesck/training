344a6af1521fc41b0d067fd3ddd5113ba9996112
ajinas <ajinas@qburst.com>
2016-08-26 16:16:34 +0530

    ajax call implemented

--- script/sample_project_page2.js
+++ script/sample_project_page2.js
@@ -1,37 +1,52 @@
 var app = angular.module(' ', ['ui.select']);
-app.controller('MyController', function($scope,Calculations) {
-     $scope.itemArray = [
-        {id: 1, name: 'first'},
-        {id: 2, name: 'second'},
-        {id: 3, name: 'third'},
-        {id: 4, name: 'fourth'}
-    ];
-
+app.controller('MyController', function($scope,$http,Calculations) {
+    $scope.loading = true;
     $scope.weektotal = [];
     $scope.total = [];
     $scope.alerting = false;
     $scope.contents = "";
+    $scope.weekDays = ["Mon","Tues","Wed","Thurs","Fri","Sat","Sun"];
+    $scope.date1 = ["3/4","3/5","3/6","3/7","3/8","3/9","3/10"];
+    $scope.date2 = ["3/11","3/12","3/13","3/14","3/15","3/16","3/17"];
+    $scope.activity1Checked = [];
+    $scope.activity2Checked = [];
  
     $scope.activities1 = [{
     	locationName: {},
     	activityName: {},
     	time: [],
-    	activityTotal:""
+    	activityTotal:"",
+    	actionArray:[],
+    	loadingAction:false
     }];
 
      $scope.activities2 = [{
     	locationName: {},
     	activityName: {},
     	time: [],
-    	activityTotal:""
+    	activityTotal:"",
+    	actionArray:[],
+    	loadingAction:false
     }];
 
+    $http.get("http://10.7.2.11:8080/mdtimesurvey/locations")
+  		.then(function(response) {
+      		console.log(response.data.data);
+      		$scope.locationArray = response.data.data;
+      		$scope.loading = false;
+    
+    }, function(response) {
+       console.log("Something went wrong");
+  	});
+   
    	$scope.addActivity = function(){
    		var activity = {
 	    	locationName: {},         
 	    	activityName: {},
 	    	time: [],
-	    	activityTotal:""
+	    	activityTotal:"",
+	    	actionArray:[],
+	    	loadingAction:false
     	};
    		$scope.activities1.push(activity);
    	}
@@ -46,7 +61,9 @@ app.controller('MyController', function($scope,Calculations) {
 	    	locationName: {},
 	    	activityName: {},
 	    	time: [],
-	    	activityTotal:""
+	    	activityTotal:"",
+	    	actionArray:[],
+	    	loadingAction:false
     	};
    		$scope.activities2.push(activity);
    	}
@@ -55,6 +72,34 @@ app.controller('MyController', function($scope,Calculations) {
 	  	$scope.activities2.splice(index,1);
 	  	$scope.evaluate2();
 	}
+	$scope.locationSelected1 =function(index){
+		$scope.activities1[index].loadingAction = true;
+		$scope.activities1[index].activityName = {};
+		$scope.activities1[index].actionArray = [];
+		 $http.get("http://10.7.2.11:8080/mdtimesurvey/loadactivities?location="+$scope.activities1[index].locationName.value)
+	  		.then(function(response) {
+	      		console.log(response.data.data);
+	      		$scope.activities1[index].actionArray = response.data.data;
+	      		$scope.activities1[index].loadingAction = false;
+	    
+	    }, function(response) {
+	       console.log("Something went wrong");
+	  	});	
+	 }
+	 $scope.locationSelected2 =function(index){
+	 	$scope.activities2[index].loadingAction = true;
+		$scope.activities2[index].activityName = {};
+		$scope.activities2[index].actionArray = [];
+		 $http.get("http://10.7.2.11:8080/mdtimesurvey/loadactivities?location="+$scope.activities2[index].locationName.value)
+	  		.then(function(response) {
+	      		console.log(response.data.data);
+	      		$scope.activities2[index].actionArray = response.data.data;
+	      		$scope.activities2[index].loadingAction = false;
+	    
+	    }, function(response) {
+	       console.log("Something went wrong");
+	  	});	
+	 }		
 
    	$scope.evaluate1 = function(){
    		$scope.activities1 = Calculations.calculateActivityTotal($scope.activities1);
@@ -124,13 +169,13 @@ app.controller('MyController', function($scope,Calculations) {
   	}
   	$scope.compareSelect = function(index){
   		if($scope.activities1[index].activityName.value !== undefined){
-	  		var selectedString = $scope.activities1[index].locationName.value.name
-	  			+$scope.activities1[index].activityName.value.name;
+	  		var selectedString = $scope.activities1[index].locationName.value
+	  			+$scope.activities1[index].activityName.value.label;
 	  		for(var i=0;i<$scope.activities1.length;i++){
 	  			if(($scope.activities1[i].locationName.value !== undefined) &&
 	  			 						($scope.activities1[i].activityName.value !== undefined) && (i !== index)){
-		  			var compareString = $scope.activities1[i].locationName.value.name+
-		  								$scope.activities1[i].activityName.value.name;
+		  			var compareString = $scope.activities1[i].locationName.value+
+		  								$scope.activities1[i].activityName.value.label;
 		  			if(selectedString === compareString){
 		  				$scope.contents = "Combination already selected";
 		  				$scope.alerting = true;
@@ -141,14 +186,14 @@ app.controller('MyController', function($scope,Calculations) {
 	  	}	
   	}
   	$scope.compareSelect2 = function(index){
-  		if($scope.activities1[index].activityName.value !== undefined){
-	  		var selectedString = $scope.activities2[index].locationName.value.name
-	  			+$scope.activities2[index].activityName.value.name;
+  		if($scope.activities2[index].activityName.value !== undefined){
+	  		var selectedString = $scope.activities2[index].locationName.value
+	  			+$scope.activities2[index].activityName.value.label;
 	  		for(var i=0;i<$scope.activities2.length;i++){
 	  				if(($scope.activities2[i].locationName.value !== undefined) &&
 	  			 						($scope.activities2[i].activityName.value !== undefined) && (i !== index)){
-		  			var compareString = $scope.activities2[i].locationName.value.name+
-		  								$scope.activities2[i].activityName.value.name;
+		  			var compareString = $scope.activities2[i].locationName.value+
+		  								$scope.activities2[i].activityName.value.label;
 		  			if(selectedString === compareString){
 		  				$scope.contents = "Combination already selected";
 		  				$scope.alerting = true;
